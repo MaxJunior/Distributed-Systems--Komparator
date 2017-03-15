052e3644bb2a89fb62f2d74abc721f180d45173b
André Tomé <andretome91@gmail.com>
2017-03-15 04:21:41 +0000

    Integration tests done for searchProducts function

--- supplier-ws-cli/src/test/java/org/komparator/supplier/ws/it/SearchProductsIT.java
+++ supplier-ws-cli/src/test/java/org/komparator/supplier/ws/it/SearchProductsIT.java
@@ -22,11 +22,43 @@ public class SearchProductsIT extends BaseIT {
 
 	// one-time initialization and clean-up
 	@BeforeClass
-	public static void oneTimeSetUp() {
+	public static void oneTimeSetUp() throws BadProductId_Exception, BadProduct_Exception{
+		// clear remote service state before all tests
+		client.clear();
+
+		// fill-in test products
+		// (since getProduct is read-only the initialization below
+		// can be done once for all tests in this suite)
+		{
+			ProductView product = new ProductView();
+			product.setId("X1");
+			product.setDesc("Basketball");
+			product.setPrice(10);
+			product.setQuantity(10);
+			client.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("Y2");
+			product.setDesc("Baseball");
+			product.setPrice(20);
+			product.setQuantity(20);
+			client.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("Z3");
+			product.setDesc("Soccer ball");
+			product.setPrice(30);
+			product.setQuantity(30);
+			client.createProduct(product);
+		}
 	}
 
 	@AfterClass
 	public static void oneTimeTearDown() {
+		// clear remote service state after all tests
+		client.clear();
 	}
 
 	// members
@@ -47,12 +79,53 @@ public class SearchProductsIT extends BaseIT {
 	// BadText_Exception
 
 	// bad input tests
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsNullTest() throws BadText_Exception {
+		client.searchProducts(null);
+	}
 
-	// TODO
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsEmptyTest() throws BadText_Exception {
+		client.searchProducts("");
+	}
+
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsWhitespaceTest() throws BadText_Exception {
+		client.searchProducts(" ");
+	}
+
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsTabTest() throws BadText_Exception {
+		client.searchProducts("\t");
+	}
+
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsNewlineTest() throws BadText_Exception {
+		client.searchProducts("\n");
+	}
 
 	
 	// main tests
+	@Test
+	public void searchProductExistsTest() throws BadText_Exception {
+		// when description contains text, non empty list should be returned
+		List<ProductView> products = client.searchProducts("soccer");
+		assertEquals(products.isEmpty(), false);
+		assertEquals(products.size(), 1);
 
-	// TODO
+		ProductView product = products.get(0);
+		assertEquals("Z3", product.getId());
+		assertEquals(30, product.getPrice());
+		assertEquals(30, product.getQuantity());
+		assertEquals("Soccer ball", product.getDesc());
+	}
+
+	@Test
+	public void searchProductNotExistsTest() throws BadText_Exception {
+		// when description does not contain text, empty list should be returned
+		List<ProductView> products = client.searchProducts("abcdefghi");
+		assertTrue(products.isEmpty());
+	}
+
 
 }
