7322a1439e6e160af8e81bedd96adedfa35ddc48
Rodolfo Cardoso <rodolfobragacardoso@hotmail.com>
2017-04-05 16:30:59 +0100

    supplier-ws with juddi

--- supplier-ws/src/main/java/org/komparator/supplier/ws/SupplierApp.java
+++ supplier-ws/src/main/java/org/komparator/supplier/ws/SupplierApp.java
@@ -1,20 +1,27 @@
 package org.komparator.supplier.ws;
 
+import javax.xml.ws.Endpoint;
+
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
 
 /** Main class that starts the Supplier Web Service. */
 public class SupplierApp {
 
 	public static void main(String[] args) throws Exception {
 		// Check arguments
-		if (args.length < 1) {
+		if (args.length < 3) {
 			System.err.println("Argument(s) missing!");
-			System.err.println("Usage: java " + SupplierApp.class.getName() + " wsURL");
+			System.err.println("Usage: java " + SupplierApp.class.getName() + " uddiURL wsName");
 			return;
 		}
-		String wsURL = args[0];
+		
+		String uddiURL = args[0];
+		String name = args[1];
+		String wsURL = args[2];
+		
 
 		// Create server implementation object
-		SupplierEndpointManager endpoint = new SupplierEndpointManager(wsURL);
+		SupplierEndpointManager endpoint = new SupplierEndpointManager(uddiURL, name, wsURL);
 		try {
 			endpoint.start();
 			endpoint.awaitConnections();
--- supplier-ws/src/main/java/org/komparator/supplier/ws/SupplierEndpointManager.java
+++ supplier-ws/src/main/java/org/komparator/supplier/ws/SupplierEndpointManager.java
@@ -4,12 +4,16 @@ import java.io.IOException;
 
 import javax.xml.ws.Endpoint;
 
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+
 
 /** End point manager */
 public class SupplierEndpointManager {
 
-	/** Web Service location to publish */
+	private String uddiURL = null;
+	private String name = null;
 	private String wsURL = null;
+	private UDDINaming uddiNaming = null;
 
 	/** Port implementation */
 	private SupplierPortImpl portImpl = new SupplierPortImpl(this);
@@ -34,11 +38,63 @@ public class SupplierEndpointManager {
 	}
 
 	/** constructor with provided web service URL */
-	public SupplierEndpointManager(String wsURL) {
-		if (wsURL == null)
-			throw new NullPointerException("Web Service URL cannot be null!");
+	public SupplierEndpointManager(String uddiURL, String name, String wsURL) {
+		if (uddiURL == null)
+			throw new NullPointerException("UDDI URL cannot be null!");
+		if(name == null)
+			throw new NullPointerException("Name cannot be null!");
+		
+		this.uddiURL = uddiURL;
+		this.name = name;
 		this.wsURL = wsURL;
+		
+		
+		try {
+			endpoint = Endpoint.create(portImpl);
+
+			// publish endpoint
+			System.out.printf("Starting %s%n", wsURL);
+			endpoint.publish(wsURL);
+
+			// publish to UDDI
+			System.out.printf("Publishing '%s' to UDDI at %s%n", name, uddiURL);
+			uddiNaming = new UDDINaming(uddiURL);
+			uddiNaming.rebind(name, wsURL);
+
+			// wait
+			System.out.println("Awaiting connections");
+			System.out.println("Press enter to shutdown");
+			System.in.read();
+
+		} catch (Exception e) {
+			System.out.printf("Caught exception: %s%n", e);
+			e.printStackTrace();
+
+		} finally {
+			try {
+				if (endpoint != null) {
+					// stop endpoint
+					endpoint.stop();
+					System.out.printf("Stopped %s%n", wsURL);
+				}
+			} catch (Exception e) {
+				System.out.printf("Caught exception when stopping: %s%n", e);
+			}
+			try {
+				if (uddiNaming != null) {
+					// delete from UDDI
+					uddiNaming.unbind(name);
+					System.out.printf("Deleted '%s' from UDDI%n", name);
+				}
+			} catch (Exception e) {
+				System.out.printf("Caught exception when deleting: %s%n", e);
+			}
+		}
+
 	}
+		
+		
+		
 
 	/* end point management */
 
@@ -74,7 +130,7 @@ public class SupplierEndpointManager {
 		}
 	}
 
-	public void stop() throws Exception {
+	public void stop() throws Exception {    //TODO
 		try {
 			if (endpoint != null) {
 				// stop end point
